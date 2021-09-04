82b50be3f96ae6b8c7090668248bf609d27e3cd6
marsen5689 <90087436+marsen5689@users.noreply.github.com>
2021-09-04 12:16:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let x = +prompt("введите X");
+let y = +prompt("введите y");
+if (y == 0) {
+    alert("Думаем.")
+    alert("Думаем..")
+    alert("Думаем...")
+    alert("Error")
+} else {
+    alert(x / y);
+}
